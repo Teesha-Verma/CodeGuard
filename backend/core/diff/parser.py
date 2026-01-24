6eddef6d58e05ea485fd0eb7a946ac40df584927
Teesha-Verma <teeshaverma1@gmail.com>
2026-01-24 17:00:58 +0530

    updated parser logic

--- backend/core/diff/parser.py
+++ backend/core/diff/parser.py
@@ -12,10 +12,10 @@ class diffParser:
         added_lines=[]
         removed_lines=[]
 
-        old_lines= None
-        new_lines=None
+        old_line_no= None
+        new_line_no=None
 
-        for line in diff_text.splitlines:
+        for line in diff_text.splitlines():
 
             # new file block
 
@@ -28,6 +28,45 @@ class diffParser:
                     })
                 
                 current_file=line.split("b/")[-1]
+                added_lines = []
+                removed_lines = []
+                old_line_no = None
+                new_line_no = None
+
+            # Hunk header
+            elif line.startswith("@@"):
+                match = self.HUNK_PATTERN.search(line)
+                if match:
+                    old_line_no = int(match.group(1))
+                    new_line_no = int(match.group(2))
+
+            # Removed line
+            elif line.startswith("-") and not line.startswith("---"):
+                removed_lines.append(old_line_no)
+                old_line_no += 1
+
+            # Added line
+            elif line.startswith("+") and not line.startswith("+++"):
+                added_lines.append(new_line_no)
+                new_line_no += 1
+
+            # Context line
+            else:
+                if old_line_no is not None:
+                    old_line_no += 1
+                if new_line_no is not None:
+                    new_line_no += 1
+
+        # Flush last file
+        if current_file:
+            result.append({
+                "file": current_file,
+                "added_lines": added_lines,
+                "removed_lines": removed_lines
+            })
+
+        return result
+                
 
 
 
